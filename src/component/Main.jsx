474a348d2ec930027856866368ee2853f1ea7df0
Millana <chotubaevam@gmail.com>
2022-04-24 23:00:54 +0600

    add pagination

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -2,13 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { callToAPI } from "./ReduxSlice";
 import Header from "./Header";
-import {
-  Grid,
-  Card,
-  Modal,
-  Box,
-  CardActionArea,
-} from "@mui/material";
+import { Grid, Card, Modal, Box, CardActionArea } from "@mui/material";
 import { debounce } from "lodash";
 
 function Main() {
@@ -28,7 +22,7 @@ function Main() {
 
   // Modal page
   const [selectedCartoon, setSelectedCartoon] = useState([]);
-  
+
   const [open, setOpen] = useState(false);
   const handleClose = () => setOpen(false);
   const handleOpen = () => setOpen(true);
@@ -57,12 +51,22 @@ function Main() {
       }
     });
     setSearched(filteredCartoons);
-    setLengthSearch(e.target.value.length)
+    setLengthSearch(e.target.value.length);
   }, 500);
- 
+
   useEffect(() => {
-    handlerOnChange()
-  }, [searched])
+    handlerOnChange();
+  }, [searched]);
+
+  // Pagination part
+  let howManyElPerPage = 6;
+  let howManyPage = Math.ceil(data.length / howManyElPerPage);
+  const arrayBtn = new Array(howManyPage).fill("btnPage");
+  const [pageNow, setPageNow] = useState(0);
+  let start = pageNow * howManyElPerPage;
+  let end = start + howManyElPerPage;
+  let dataCopy = [...data]
+  const pagination = dataCopy.slice(start, end);
   // https://logolook.net/wp-content/uploads/2021/07/Nickelodeon-Logo.svg
   // https://static.cdnlogo.com/logos/n/16/nickelodeon.svg
   return (
@@ -79,119 +83,133 @@ function Main() {
             <input
               className="input-search"
               onChange={(e) => handlerOnChange(e)}
+              placeholder="Search your favourite cartoon"
             />
           </div>
           {lengthSearch > 1 ? (
             <>
-            <Grid container spacing={2}>
-            {searched?.map((el, index) => (
-              <>
-                <Grid
-                  item
-                  xs={3}
-                  md={2}
-                  sx={{ textAlign: "center" }}
-                  key={index}
-                  onClick={handleOpen}
-                >
-                  <Card
-                    sx={{ maxWidth: 300 }}
-                    onClick={() => setSelectedCartoon(el)}
+              <Grid container spacing={2}>
+                {searched?.map((el, index) => (
+                  <>
+                    <Grid
+                      item
+                      xs={3}
+                      md={2}
+                      sx={{ textAlign: "center" }}
+                      key={index}
+                      onClick={handleOpen}
+                    >
+                      <Card
+                        sx={{ maxWidth: 300 }}
+                        onClick={() => setSelectedCartoon(el)}
+                      >
+                        <CardActionArea>
+                          <img src={el.image} height="260px" />
+                          <h3 style={{ color: "#fbad00" }}>{el.title}</h3>
+                          <p>
+                            Program creator: <h4>{el.creator[0]}</h4>
+                          </p>
+                        </CardActionArea>
+                      </Card>
+                    </Grid>
+                  </>
+                ))}
+                <div>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
                   >
-                    <CardActionArea>
-                      <img src={el.image} height="260px" />
-                      <h3 style={{ color: "#fbad00" }}>{el.title}</h3>
-                      <p>
-                        Program creator: <h4>{el.creator[0]}</h4>
-                      </p>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              </>
-            ))}
-            <div>
-              <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              >
-                <Box sx={style}>
-                  <img src={selectedCartoon.image} height="250px" />
-                  <h2>{selectedCartoon.title}</h2>
-                  <div className="element-flex">
-                    <p>First episode year: </p>
-                    <h3> {selectedCartoon.year}</h3>
-                  </div>
-                  <div className="element-flex">
-                    <p>Episodes: </p>
-                    <h3>{selectedCartoon.episodes}</h3>
-                  </div>
-                  <div className="element-flex">
-                    <p>One episode time:</p>
-                    <h3>{selectedCartoon.runtime_in_minutes}</h3>
-                  </div>
-                </Box>
-              </Modal>
-            </div>
-          </Grid>
+                    <Box sx={style}>
+                      <img src={selectedCartoon.image} height="250px" />
+                      <h2>{selectedCartoon.title}</h2>
+                      <div className="element-flex">
+                        <p>First episode year: </p>
+                        <h3> {selectedCartoon.year}</h3>
+                      </div>
+                      <div className="element-flex">
+                        <p>Episodes: </p>
+                        <h3>{selectedCartoon.episodes}</h3>
+                      </div>
+                      <div className="element-flex">
+                        <p>One episode time:</p>
+                        <h3>{selectedCartoon.runtime_in_minutes}</h3>
+                      </div>
+                    </Box>
+                  </Modal>
+                </div>
+              </Grid>
             </>
           ) : (
             <>
-            <Grid container spacing={2}>
-            {data?.map((el, index) => (
-              <>
-                <Grid
-                  item
-                  xs={3}
-                  md={2}
-                  sx={{ textAlign: "center" }}
-                  key={index}
-                  onClick={handleOpen}
-                >
-                  <Card
-                    sx={{ maxWidth: 300 }}
-                    onClick={() => setSelectedCartoon(el)}
+              <Grid container spacing={2}>
+                {pagination?.map((el, index) => (
+                  <>
+                    <Grid
+                      item
+                      xs={3}
+                      md={2}
+                      sx={{ textAlign: "center" }}
+                      key={index}
+                      onClick={handleOpen}
+                    >
+                      <Card
+                        sx={{ maxWidth: 300 }}
+                        onClick={() => setSelectedCartoon(el)}
+                      >
+                        <CardActionArea>
+                          <img src={el.image} height="260px" />
+                          <h3 style={{ color: "#fbad00" }}>{el.title}</h3>
+                          <p>
+                            Program creator: <h4>{el.creator[0]}</h4>
+                          </p>
+                        </CardActionArea>
+                      </Card>
+                    </Grid>
+                  </>
+                ))}
+                <div>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
                   >
-                    <CardActionArea>
-                      <img src={el.image} height="260px" />
-                      <h3 style={{ color: "#fbad00" }}>{el.title}</h3>
-                      <p>
-                        Program creator: <h4>{el.creator[0]}</h4>
-                      </p>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              </>
-            ))}
-            <div>
-              <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              >
-                <Box sx={style}>
-                  <img src={selectedCartoon.image} height="250px" />
-                  <h2>{selectedCartoon.title}</h2>
-                  <div className="element-flex">
-                    <p>First episode year: </p>
-                    <h3> {selectedCartoon.year}</h3>
-                  </div>
-                  <div className="element-flex">
-                    <p>Episodes: </p>
-                    <h3>{selectedCartoon.episodes}</h3>
-                  </div>
-                  <div className="element-flex">
-                    <p>One episode time:</p>
-                    <h3>{selectedCartoon.runtime_in_minutes}</h3>
-                  </div>
-                </Box>
-              </Modal>
-            </div>
-          </Grid></>
+                    <Box sx={style}>
+                      <img src={selectedCartoon.image} height="250px" />
+                      <h2>{selectedCartoon.title}</h2>
+                      <div className="element-flex">
+                        <p>First episode year: </p>
+                        <h3> {selectedCartoon.year}</h3>
+                      </div>
+                      <div className="element-flex">
+                        <p>Episodes: </p>
+                        <h3>{selectedCartoon.episodes}</h3>
+                      </div>
+                      <div className="element-flex">
+                        <p>One episode time:</p>
+                        <h3>{selectedCartoon.runtime_in_minutes}</h3>
+                      </div>
+                    </Box>
+                  </Modal>
+                </div>
+              </Grid>
+              <div className="btnPageAll">
+                {arrayBtn.map((el, index) => {
+                  return (
+                    <button
+                      className="btnPage"
+                      onClick={() => setPageNow(index)}
+                      key={index + el}
+                    >
+                      {index + 1}
+                    </button>
+                  );
+                })}
+              </div>
+            </>
           )}
-          
         </>
       )}
     </>
